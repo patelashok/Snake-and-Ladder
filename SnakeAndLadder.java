224cd6f56ee12bb6c0bbc7e98e19cb91d36e3eb7
patelashok <ashok.patel2917@gmail.com>
2021-05-14 12:52:24 +0530

    roll die get number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,23 @@
-
+import java.util.*;
 
 	public class SnakeAndLadder 
 	{	
 		public static void main(String[] args) 
 		{
 			System.out.println("Welcome to SnakeAndLadder Game");
-		
+
 			int position=0;
 			System.out.println("Player start position : "+position);
+                        Random random = new Random();
+ 			
+			int dice = 0;
+			while (true)
+			{
+	    		dice = random.nextInt(7);
+	    		if(dice !=0) break;
+			}
+			System.out.println(dice);
+
+
 		}
 	} 
