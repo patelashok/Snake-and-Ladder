16bdea8d502e5a0aa818b19850ffa42a72416d61
patelashok <ashok.patel2917@gmail.com>
2021-05-14 12:40:26 +0530

    sinlge player at start position zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,5 +5,8 @@
 		public static void main(String[] args) 
 		{
 			System.out.println("Welcome to SnakeAndLadder Game");
+		
+			int position=0;
+			System.out.println("Player start position : "+position);
 		}
 	} 
